9db1b5a23cf8ea58c5797816bacb5e7f210fbfb8
SebastianCP <sebastian.cardozo@neotech.co>
2024-08-31 19:47:02 -0500

    Ajuste Summaries

--- ApiHelm/Controllers/WeatherForecastController.cs
+++ ApiHelm/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ApiHelm.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Congelante", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Congelante", "Vigorizante", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -18,7 +18,7 @@ namespace ApiHelm.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast2")]
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
